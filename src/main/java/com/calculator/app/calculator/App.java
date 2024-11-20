a8e0b90b93842eba6816be509743a56a9002a0a1
Bhaktilata Dash <dashbhakti1998@gmail.com>
2024-11-20 13:58:56 +0530

    calculatocommit-1

---
+++ src/main/java/com/calculator/app/calculator/App.java
@@ -0,0 +1,16 @@
+package com.calculator.app.calculator;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static void add( String data )
+    
+    {
+    	
+        System.out.println( "data ");
+    }
+}
