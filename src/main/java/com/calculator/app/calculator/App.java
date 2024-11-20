746842c5af113fb2b7945f8fe5a6aad9df954bc9
Bhaktilata Dash <dashbhakti1998@gmail.com>
2024-11-21 01:10:36 +0530

    added 2nd time

--- src/main/java/com/calculator/app/calculator/App.java
+++ src/main/java/com/calculator/app/calculator/App.java
@@ -6,6 +6,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Hello world 2");
     }
     public static void add( String data )
     
